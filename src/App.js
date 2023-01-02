d2ffba51e01f1e517b82466c413fe4560c509da1
Akianonymus <anonymus.aki@gmail.com>
2023-01-02 16:10:02 +0530

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
+
+const expenses = [
+  {
+    id: "1",
+    title: "Netflix",
+    amount: 69,
+    date: new Date(2010, 7, 20),
+  },
+  { id: "2", title: "Laptop", amount: 799.49, date: new Date(2022, 6, 12) },
+  {
+    id: "3",
+    title: "Washing machine",
+    amount: 400.67,
+    date: new Date(2020, 9, 1),
+  },
+  {
+    id: "4",
+    title: "Hair Dryer",
+    amount: 100,
+    date: new Date(2023, 1, 1),
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Expense Tracker</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
